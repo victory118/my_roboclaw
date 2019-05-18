f2a0c2aa6b305e80d194fb84088855f456e9405d
Victor Yu <victory118@gmail.com>
2019-05-18 12:07:18 -0400

    added new roboclaw motor test python script

---
+++ scripts/roboclaw_motor_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from roboclaw_drive.roboclaw import Roboclaw
+
+if __name__ == '__main__':
+    address = 0x80
+    roboclaw = Roboclaw("/dev/ttyS0", 38400)
+    roboclaw.Open()
+
+    roboclaw.ForwardM1(address,64)
+    sleep(2)
+    roboclaw.ForwardM1(address,0)
+    sleep(2)
+
+    roboclaw.BackwardM1(address,64)
+    sleep(2)
+    roboclaw.BackwardM1(address,0)
+    sleep(2)
+        
+    roboclaw.ForwardM2(address, 64)
+    sleep(2)
+    roboclaw.ForwardM2(address,0)
+    sleep(2)
+
+    roboclaw.BackwardM2(address, 64)
+    sleep(2)
+    roboclaw.BackwardM2(address,0)
+    sleep(2)
